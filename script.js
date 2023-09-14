cef3d3c81d487dbefd60398fcc4e047c6637be27
Torje Amundsen <torjefrafen@hotmail.no>
2023-09-14 11:42:46 +0200

    Updates

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const app = document.getElementById("app");
 const playerElement = document.getElementById("player");
 const itemsContainer = document.getElementById("items-container");
+const healthBar = document.getElementById("health-progress");
+const coolnessBar = document.getElementById("coolness-progress");
 
 const model = {
   itemsSpawned: 0,
@@ -11,7 +13,8 @@ const model = {
   itemSpawnCooldown: false,
   timeoutIds: [],
   player: {
-    coolness: 0,
+    coolness: 25,
+    health: 100,
     upgrades: [],
     money: 0,
     movementSpeed: 0,
@@ -28,13 +31,15 @@ const model = {
     { name: "Banana", coolness: -20, money: 0, damage: 5, class: "banana" },
     { name: "Screw", coolness: 5, money: 0, damage: -25, class: "screw" },
     { name: "Money", coolness: 5, money: 150, damage: 0, class: "money" },
+    { name: "Hole", coolness: -40, money: 0, damage: 30, class: "hole" },
+    { name: "Rival", coolness: -20, money: 0, damage: 0, class: "rival" },
   ],
   homieTable: [
     {
       id: 0,
       name: "Big Man",
       greetingId: 0,
-      wrongGreetingMessage: "Dafuq man, don't you know the greeting?",
+      wrongGreetingMessage: "Dafuq man, don't you know how to greet me?",
       correctGreetingMessage: "Yo my man, wassup",
     },
     {
@@ -48,8 +53,8 @@ const model = {
       id: 2,
       name: "Crackhead",
       greetingId: 2,
-      wrongGreetingMessage: "",
-      correctGreetingMessage: "",
+      wrongGreetingMessage: "Ayo, have you forgotten it?",
+      correctGreetingMessage: "Cool bro, I'll lend you the crack pipe later.",
     },
   ],
   shopItemsTable: [
@@ -64,8 +69,6 @@ const model = {
   enemiesTable: [
     { id: 0, name: "GrandMa", damage: 30 },
     { id: 1, name: "politi", damage: 40 },
-    { id: 2, name: "birdPoo", damage: 10 },
-    { id: 3, name: "rival", damage: 20 },
   ],
 };
 
@@ -73,16 +76,28 @@ const model = {
 
 function updateView() {
   playerElement.style.left = `${model.player.position}px`;
+  healthBar.style.height = getHeight(model.player.health);
+  coolnessBar.style.height = getHeight(model.player.coolness);
   for (const item of model.activeItems) {
     itemDiv = document.getElementById(item.id);
     if (itemDiv) {
-      itemDiv.style.top = `${item.position}px`;
+      itemDiv.style.top = `${item.yPosition}px`;
+      itemDiv.style.left = `${item.xPosition}px`;
     } else {
       itemsContainer.innerHTML += /*html*/ `
-        <div id="${item.id}" class="${item.class}" style="top: -80px;"></div>
+        <div id="${item.id}" class="item ${item.class}" style="top: -80px;"></div>
       `;
     }
   }
+}
+
+function getHeight(value) {
+  let percentage = value / 100;
+  return `${percentage * 400}px`;
+}
+
+function removeItemElement(id) {
+  document.getElementById(id).remove();
 }
 
 // Controller
@@ -90,10 +105,10 @@ function updateView() {
 function interact() {}
 
 function handleKeyDown(event) {
-  if (event.key === "a") {
+  if (event.key === "a" || event.key === "ArrowLeft") {
     model.player.holdingLeft = true;
   }
-  if (event.key === "d") {
+  if (event.key === "d" || event.key === "ArrowRight") {
     model.player.holdingRight = true;
   }
   if (event.key === "f") {
@@ -102,31 +117,59 @@ function handleKeyDown(event) {
 }
 
 function handleKeyUp(event) {
-  if (event.key === "a") {
+  if (event.key === "a" || event.key === "ArrowLeft") {
     model.player.holdingLeft = false;
   }
-  if (event.key === "d") {
+  if (event.key === "d" || event.key === "ArrowRight") {
     model.player.holdingRight = false;
   }
 }
-
 function getRandomNumFromZero(max) {
   return Math.floor(Math.random() * max);
 }
 
 function updateActiveItems() {
   for (let i = 0; i < model.activeItems.length; i++) {
-    if (!model.activeItems[i].position && model.activeItems[i] !== 0) {
+    if (!model.activeItems[i].yPosition && model.activeItems[i].yPosition !== 0) {
       // Todo: generate a random horizontal position
-      model.activeItems[i].position = -80;
+      model.activeItems[i].yPosition = -80;
+      model.activeItems[i].xPosition = Math.floor(Math.random() * (620 - 20 + 1) + 20);
       model.activeItems[i].id = `item-${model.itemsSpawned}`;
     }
-    model.activeItems[i].position += 4;
-    if (model.activeItems[i].position >= window.innerHeight) {
+    model.activeItems[i].yPosition += 5;
+    if (model.activeItems[i].yPosition >= window.innerHeight) {
+      removeItemElement(model.activeItems[i].id);
       model.activeItems.splice(i, 1);
+    } else {
+      const itemVSPlayerPosition = model.activeItems[i].xPosition - model.player.position;
+      const PLAYER_WIDTH = 160;
+      const PLAYER_HEIGHT_START = 674;
+      const PLAYER_HEIGHT_END = 898;
+      const ITEM_SIZE = 80;
+      if (
+        itemVSPlayerPosition >= -ITEM_SIZE &&
+        itemVSPlayerPosition <= PLAYER_WIDTH &&
+        model.activeItems[i].yPosition >= PLAYER_HEIGHT_START - ITEM_SIZE &&
+        model.activeItems[i].yPosition <= PLAYER_HEIGHT_END
+      ) {
+        let newHealth = model.player.health - model.activeItems[i].damage;
+        let newCoolness = model.player.coolness + model.activeItems[i].coolness;
+        if (newHealth > 100) {
+          newHealth = 100;
+        }
+        if (newCoolness > 100) {
+          newCoolness = 100;
+        } else if (newCoolness < 0) {
+          newCoolness = 0;
+        }
+        model.player.health = newHealth;
+        model.player.coolness = newCoolness;
+
+        removeItemElement(model.activeItems[i].id);
+        model.activeItems.splice(i, 1);
+      }
     }
   }
-  console.log(model.activeItems);
 }
 
 function spawnRandomItem() {
@@ -139,7 +182,7 @@ function spawnRandomItem() {
     setTimeout(() => {
       model.itemSpawnCooldown = false;
       model.timeoutIds.pop();
-    }, 3000)
+    }, 1200)
   );
   model.itemsSpawned++;
   model.activeItems.push(JSON.parse(JSON.stringify(model.itemTable[randomIndex])));
